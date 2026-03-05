0eff729f8655c2a714d4fbd3c4f6e8c56dfcb119
kadam-x <sn33zyhub@gmail.com>
2026-03-05 18:49:46 +0100

    base16-bmg theme

---
+++ nvim/.config/nvim/lua/plugins/base16-bmg.lua
@@ -0,0 +1,36 @@
+return {
+	"RRethy/base16-nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("base16-colorscheme").setup({
+			base00 = "#000000",
+			base01 = "#121212",
+			base02 = "#222222",
+			base03 = "#333333",
+			base04 = "#999999",
+			base05 = "#c1c1c1",
+			base06 = "#999999",
+			base07 = "#c1c1c1",
+			base08 = "#5f8787",
+			base09 = "#aaaaaa",
+			base0A = "#8c7f70",
+			base0B = "#9b8d7f",
+			base0C = "#aaaaaa",
+			base0D = "#888888",
+			base0E = "#999999",
+			base0F = "#444444",
+		})
+
+		-- Overrides
+		vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#912222" })
+		vim.api.nvim_set_hl(0, "TSComment", { fg = "#6f7b68" })
+		vim.api.nvim_set_hl(0, "Comment", { fg = "#6f7b68" })
+		vim.api.nvim_set_hl(0, "Visual", { bg = "#9b8d7f", fg = "#1e1e1e" })
+		vim.api.nvim_set_hl(0, "Search", { bg = "#9b8d7f", fg = "#1e1e1e" })
+		vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#9b8d7f", fg = "#1e1e1e" })
+		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#888888", bg = "#222222" })
+		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#888888", bg = "#222222" })
+		vim.api.nvim_set_hl(0, "LineNr", { fg = "#d6d2c8" })
+	end,
+}
