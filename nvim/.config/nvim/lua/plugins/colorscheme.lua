16f7bfc3ed012b2363b1a05c40110865ebcd3301
kadam-x <contact.jumbo009@passinbox.com>
2026-09-09 16:39:29 +0200

    riverwm, waybar

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,39 +1,17 @@
+local function enable_transparency()
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+end
+
 return {
-    "vague26/vague.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("vague").setup({
-            transparent = false,
-            bold = false,
-            italic = false,
-            on_highlights = function(hl, colors) end,
-            colors = {
-                bg = "#141415",
-                inactiveBg = "#1c1c24",
-                fg = "#cdcdcd",
-                floatBorder = "#878787",
-                line = "#252530",
-                comment = "#606079",
-                builtin = "#b4d4cf",
-                func = "#c48282",
-                string = "#e8b589",
-                number = "#e0a363",
-                property = "#c3c3d5",
-                constant = "#aeaed1",
-                parameter = "#bb9dbd",
-                visual = "#333738",
-                error = "#d8647e",
-                warning = "#f3be7c",
-                hint = "#7e98e8",
-                operator = "#90a0b5",
-                keyword = "#6e94b2",
-                type = "#9bb4bc",
-                search = "#405065",
-                plus = "#7fa563",
-                delta = "#f3be7c",
-            },
-        })
-        vim.cmd("colorscheme vague")
-    end,
+	{
+		"folke/tokyonight.nvim",
+		config = function()
+			vim.cmd.colorscheme("tokyonight")
+			vim.cmd("hi Directory guibg=NONE")
+			vim.cmd("hi SignColumn guibg=NONE")
+			enable_transparency()
+		end,
+	},
 }
